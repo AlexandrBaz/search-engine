7a8e2b355b5bc83b2625f31e3af0b60f4956dc0e
ABazh <alexbalex1@yandex.ru>
2023-02-10 14:46:15 +0400

    change application.properties to yaml format

--- src/main/java/searchengine/config/SitesList.java
+++ src/main/java/searchengine/config/SitesList.java
@@ -2,7 +2,6 @@ package searchengine.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +12,5 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "indexing-settings")
 public class SitesList {
-    @Value("#{${sites}}")
     private List<Site> sites;
 }
